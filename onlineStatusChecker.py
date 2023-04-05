3550d2cb9e08b12928ce6e98a69d61b23e8579d0
fatm1nd <semeritskiy@yandex.ru>
2023-04-05 12:38:50 +0300

    Add "bot" feature. Echo the info about alias

--- onlineStatusChecker.py
+++ onlineStatusChecker.py
@@ -9,6 +9,7 @@ client = telethon.TelegramClient(
 
 
 
+
 async def main():
     while True:
         name = input("Enter nickname:")
@@ -34,7 +35,36 @@ async def _getStatus(name, timezone='Europe/Moscow'):
 #     client.loop.run_until_complete(main())
 
 
+
 def getStatus(name, timezone='Europe/Moscow'):
     client.start()
     with client:
         return client.loop.run_until_complete(_getStatus(name, timezone='Europe/Moscow'))
+    
+
+
+
+@client.on(telethon.events.NewMessage)
+async def echo(event):
+    # Check if the message was sent by a user (not a bot)
+        # Send back the same message to the user
+    info = await client.get_entity(event.message.text)
+    if type(info.status) == telethon.tl.types.UserStatusOffline:
+        date = info.status.was_online
+        date = date.astimezone(pytz.timezone('Europe/Moscow'))
+        await event.respond(f"Was online at {date}")
+    elif type(info.status) == telethon.tl.types.UserStatusOnline:
+        await event.respond("User is online!")
+    elif type(info.status) == telethon.tl.types.UserStatusRecently:
+        await event.respond("Secret motherfucker!")
+    else:
+        await event.respond("Was not online for so long...")
+    await client.send_read_acknowledge(event.message.peer_id.user_id, event.message)
+
+# Start the client
+def handlerStart():
+    with client:
+        # Run the client until it's disconnected
+        client.run_until_disconnected()
+
+handlerStart()
