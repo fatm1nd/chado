f38c6c5bb0d7b9f22eb03852895e7e93e5a385c5
fatm1nd <semeritskiy@yandex.ru>
2023-04-25 17:17:13 +0300

    Add telenews class. Add simple posts parser

--- telenews.py
+++ telenews.py
@@ -7,49 +7,115 @@ import requests
 import xml.etree.ElementTree as ET
 import re
 from bs4 import BeautifulSoup
+import re
 
 
-response = requests.get("https://t.me/s/sharemed",headers={"Accept": 'application/json'})
+response = requests.get("https://t.me/s/sharemed",
+                        headers={"Accept": 'application/json'})
 
-open("sharedme.txt","w").write(response.content.decode('utf-8'))
+open("sharedme.txt", "w").write(response.content.decode('utf-8'))
 
 
 # main = objectify.fromstring(response.content[1:])
 
-main = BeautifulSoup(response.content.decode('utf-8'),features='lxml')
+# main = BeautifulSoup(response.content.decode('utf-8'), features='lxml')
 
-posts = (main.find_all('div',attrs={"class":"tgme_widget_message_wrap"}))
+# posts = (main.find_all('div', attrs={"class": "tgme_widget_message"}))
 
-for post in posts:
-    print("--------------")
-    messages = post.find_all('div', attrs={'class':'tgme_widget_message_text'})
-    print(messages)
-    for message in messages:
-        print(message.text, end="\n\n")
+# alias = "https://t.me/s/sharemed"[15:]
+# print(alias)
 
-def is_telegram_link(link):
-        # Regular expression pattern for Telegram link
-        pattern = r'^https:\/\/t\.me\/s\/[a-zA-Z0-9_]{5,32}$'
+
+
+# for post in posts:
+#     print(f"-------{post['data-post']}-------")
+
+#     reg = "(?:\(['\"]?)(.*?)(?:['\"]?\))"
+
+#     textMessages = post.find_all(
+#         'div', attrs={'class': 'tgme_widget_message_text'})
+#     pictures = post.find_all('a', attrs={'class': 'tgme_widget_message_photo_wrap'})
+#     # print(*pictures, sep="\n")
+#     for picture in pictures:
+#         urlP = re.search(reg,picture['style']).group(1)
+#         print(urlP)
+
+#     print("   --Videos--")
+#     for video in post.find_all('video'):
+#         print(video['src'])
+#     print()
+#     for message in list(set(textMessages)):
+#         print(message.text, end="\n\n")
+
         
-        # Check if the link matches the pattern
-        if re.match(pattern, link):
-            return True
-        else:
-            return False
+
 
 class teleChannel:
 
-    def __init__(url) -> None:
-        if not is_telegram_link(url):
+    '''
+
+    '''
+
+    url = ""
+    channelPic = ""
+    title = ""
+
+    posts = {}
+
+    def __init__(self, url) -> None:
+        if not self.is_telegram_link(url):
             raise Exception("This is not link of telegram link")
+        self.url = url
+
+        response = requests.get("https://t.me/s/sharemed",
+                        headers={"Accept": 'application/json'})
+
+        main = BeautifulSoup(response.content.decode('utf-8'), features='lxml')
+
+        self.channelPic = (main.find('img'))
+
+        self.title = (main.find('div', attrs={"class":"tgme_channel_info_header_title"}).text)
+
+        postsRaw = (main.find_all('div', attrs={"class": "tgme_widget_message"}))
+        
+        alias = url[15:]
 
 
-    def is_telegram_link(link):
+        # print(alias)
+        for post in postsRaw:
+            # print(f"-------{post['data-post']}-------")
+            postId = post['data-post'][len(alias) + 1:]
+            reg = "(?:\(['\"]?)(.*?)(?:['\"]?\))"
+            self.posts[postId] = {}
+            textMessages = post.find_all(
+                'div', attrs={'class': 'tgme_widget_message_text'})
+            pictures = post.find_all('a', attrs={'class': 'tgme_widget_message_photo_wrap'})
+            # print(*pictures, sep="\n")
+            self.posts[postId]['pictures'] = []
+            for picture in pictures:
+                self.posts[postId]['pictures'].append(re.search(reg,picture['style']).group(1))
+            self.posts[postId]['videos'] = []
+            for video in post.find_all('video'):
+                self.posts[postId]['videos'].append(video['src'])
+            for message in list(set(textMessages)):
+                self.posts[postId]['text'] = message.text
+                # print(message.text, end="\n\n")
+
+
+    def is_telegram_link(self,link):
         # Regular expression pattern for Telegram link
         pattern = r'^https:\/\/t\.me\/s\/[a-zA-Z0-9_]{5,32}$'
-        
+
         # Check if the link matches the pattern
         if re.match(pattern, link):
             return True
         else:
             return False
+    
+    # def getLastPost(self):
+    #     return self.posts[0]
+
+
+
+tch = teleChannel("https://t.me/s/sharemed")
+print(tch.posts)
